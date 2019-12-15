8edd9711ef940a87fe93c2d7bd057c023eb77f12
XL P <921417536@qq.com>
2019-12-15 15:36:54 +0800

    修复了先序遍历的bug

--- src/core/Vertex.java
+++ src/core/Vertex.java
@@ -1,6 +1,6 @@
 package core;
 
-public class Vertex implements GraphComponent, Cloneable {
+public class Vertex implements GraphComponent {
     public int ui_x, ui_y;
     public String name;
     public int inx;
@@ -14,14 +14,5 @@ public class Vertex implements GraphComponent, Cloneable {
         this.inx = inx;
     }
 
-    @Override
-    protected Vertex clone() {
-        Vertex v = new Vertex();
-        v.ui_x = ui_x;
-        v.ui_y = ui_y;
-        v.name = name;
-        v.inx = inx;
-        v.selected = selected;
-        return v;
-    }
+
 }
